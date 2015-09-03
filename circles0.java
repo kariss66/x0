f1e3f8b8a1156ef443af71545e2f227cd53a8293
kariss66 <saif_karimi@yahoo.com>
2015-09-02 21:10:24 -0400

    change made
    good work

--- circles0.java
+++ circles0.java
@@ -2,6 +2,25 @@
 
 // Setup:  screen size. //
 void setup() {
+  
+  //// Wild circles #0.
+
+// Setup:  screen size. //
+void setup() {
+  size(100, 200);                // Try changing the width & height.
+}
+
+// Next frame:  circle at (x,y). //
+void draw() {
+  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  
+// ( hi bro );
+
+
+
+}
+
+
   size(200, 200);                // Try changing the width & height.
 }
 
