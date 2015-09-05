03beedf14fc83027f490ecc8c4b230906b70fc77
kariss66 <saif_karimi@yahoo.com>
2015-09-05 14:23:28 -0400

    09/05/15 sk
    changed made

--- circles0.java
+++ circles0.java
@@ -9,3 +9,14 @@ void setup() {
 void draw() {
   ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
 }
+//// Wild circles #0.
+
+// Setup:  screen size. //
+void setup() {
+  size(500, 500);                // Try changing the width & height.
+}
+
+// Next frame:  circle at (x,y). //
+void draw() {
+  ellipse( 80,80, 50,50 );       // Try changing the (x,y) and size.
+}
